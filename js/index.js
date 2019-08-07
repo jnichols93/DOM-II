7767cf57346386ff0fd1f8204e78d63bdd87a1f7
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-06 20:19:50 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,35 +11,11 @@ const paragraphs = document.querySelectorAll('p');
 const buttons = document.querySelectorAll('.btn');
 const firstHeader = document.querySelector('.text-content h2');
 const firstSection = document.querySelector('section');
-
+const header2 = document.querySelector('.content-destination h2');
+const header3= document.querySelector('.content-section .text-content h2');
 ///event listners
 
-navLogo.addEventListener("mouseenter", function () {
-    this.classList.add('nav-logo-border');
-
-});
-
-navLogo.addEventListener("mouseleave", function() {
-    this.classList.remove('nav-logo-border');
-
-});
-
-
-
-
-window.addEventListener('wheel', function() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-        navLogo.style.display = 'none';
-        navMain.style.justifyContent = 'center';
-    } else {
-        navLogo.style.cssText = ""
-        navMain.style.justifyContent = "space-between";
-    }
-})
-
-window.addEventListener('load', function() {
-    this.alert('Welcome to the Fun Bus!');
-})
+//click
 
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('click', function() {
@@ -47,60 +23,56 @@ for (let i = 0; i < navLinks.length; i++) {
     });
 };
 
+//Load
+window.addEventListener('load', function() {
+    this.alert('Welcome to the Fun Bus!');
+});
+
+//mouseovers
+header2.addEventListener('mouseover', ()=>{
+    header2.textContent = 'OR MAYBE SOME PIZZA';
+});
+header3.addEventListener('mouseover',()=>{
+    header3.textContent ='Pad Thai?';
+});
+//dblclick
 for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('dblclick', function() {
         this.style.color = '#212529';
     });
 };
-
+//resize
 window.addEventListener('resize', function() {
     topNav.style.backgroundColor = '#17A2B8';
-})
+});
+//keydown
+wholePage.addEventListener('keydown',()=>{
+    wholePage.style.backgroundColor = '#17A2B8';
+    
+});
 
+//drag start
 for (let i = 0; i < images.length; i++) {
     images[i].addEventListener('dragstart', function() {
         for (let j = 0; j < paragraphs.length; j++) {
             paragraphs[j].style.color = '#17A2B8';
         }
-    })
-}
+    })};
+    //drag end
+    for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener('dragend', function() {
+            for (let j = 0; j < paragraphs.length; j++) {
+                paragraphs[j].style.color = '#212529';
+            }
+        })
+    };
+    //prop
+    firstHeader.addEventListener('click', function(event) {
+        alert("This event won't propagate.");
+        event.stopPropagation();
+    });
+//right click
 
-for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener('dragend', function() {
-        for (let j = 0; j < paragraphs.length; j++) {
-            paragraphs[j].style.color = '#212529';
-        }
-    })
-}
-
-buttons.forEach(function(x) {
-    x.addEventListener('click', function() {
-        alert('Sorry, that page is under construction. Check back soon!');
-    })
+header2.addEventListener('contextmenu', e => {
+  e.preventDefault();
 });
-
-
-images[2].addEventListener('mousedown', function() {
-    alert('visit today Don\'t you wish you were there?');
-})
-navLinks.forEach(function(links) {
-    links.addEventListener('click', function(event) {
-        event.preventDefault();
-    })
-})
-
-firstHeader.addEventListener('click', function(event) {
-    alert("This event won't propagate.");
-    event.stopPropagation();
-})
-firstSection.addEventListener('click', function() {
-    this.style.backgroundColor = 'aqua';
-})
-
-firstSection.addEventListener('dblclick', function() {
-    this.style.cssText = '';
-})
-intro.addEventListener('select', (event) => {
-   
-
- });
